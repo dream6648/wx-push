d6c22ea55477e7f2139309eb6a29734c4542a53a
dream6648 <114422247+dream6648@users.noreply.github.com>
2022-12-29 19:54:20 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,9 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
-
+  appid: wx2ff0670504981161",
+  appsecret: "4dfa96c16cb9ca94224c61db97b34876",
+  template_id: "s4MKQyOQf_dWpIGQLqeUDMjlLmPCPHBNNKt3aGs_Aw4",
+  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk", 	s4MKQyOQf_dWpIGQLqeUDMjlLmPCPHBNNKt3aGs_Aw4
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
+  city: "菏泽",
   first_date: "2022-11-10",
 };
